0e1d60f041616e95ff07d4df90f9efe8de003389
wanderson aragão <wanderson.eq2011@gmail.com>
2023-11-05 11:51:43 -0300

    refactor: improving code

--- tests/data/usecases/authentication/remote-authentication.spec.ts
+++ tests/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,16 +1,20 @@
-import { RemoteAuthentication } from './../../../../src/data/usecases/authentication/remote-authentication';
-import { HttpPostClientSpy } from '../mocks/mock-http-client';
+import { RemoteAuthentication } from './../../../../src/data/usecases/authentication/remote-authentication'
+import { HttpPostClientSpy } from '../mocks/mock-http-client'
 
 describe('RemoteAuthentication', () => {
+  let sut: RemoteAuthentication
+  let url: string
+  let httpPostClient: HttpPostClientSpy
   beforeAll(() => {
+    url = 'any_url'
+    httpPostClient = new HttpPostClientSpy()
   })
-
+  beforeEach(() => {
+    jest.clearAllMocks()
+    sut = new RemoteAuthentication(url, httpPostClient)
+  })
   it('Should call HttpClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClient = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClient)
     await sut.auth()
     expect(httpPostClient.url).toBe(url)
   })
 })
-
