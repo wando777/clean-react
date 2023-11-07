ab58e981308d0741b70ba6e5cddf5279fa319074
wanderson aragão <wanderson.eq2011@gmail.com>
2023-11-07 08:32:36 -0300

    refactor: add generic types to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,17 +1,24 @@
-import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
-import { HttpStatusCode, HttpPostClient } from '@/data/protocols/http';
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { HttpStatusCode, HttpPostClient } from '@/data/protocols/http'
+import { AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '@/domain/models'
 
 export class RemoteAuthentication {
   constructor(
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient
-  ) { }
-  async auth(params: { email: string, password: string }): Promise<void> {
-    const response = await this.httpPostClient.post({ url: this.url, body: params })
+    private readonly httpPostClient: HttpPostClient<
+      AuthenticationParams,
+      AccountModel
+    >
+  ) {}
+  async auth(params: { email: string; password: string }): Promise<void> {
+    const response = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
     if (response.statusCode === HttpStatusCode.unauthorized) {
       throw new InvalidCredentialsError()
-    }
-    else if (response.statusCode !== HttpStatusCode.ok) {
+    } else if (response.statusCode !== HttpStatusCode.ok) {
       throw new UnexpectedError()
     }
     return await Promise.resolve()
--- tests/data/usecases/mocks/mock-http-client.ts
+++ tests/data/usecases/mocks/mock-http-client.ts
@@ -5,14 +5,13 @@ import {
   HttpStatusCode
 } from '@/data/protocols/http'
 
-
-export class HttpPostClientSpy implements HttpPostClient {
+export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
-  body?: object
-  response: HttpResponse = {
+  body?: T
+  response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok
   }
-  post(params: HttpPostParams): Promise<HttpResponse> {
+  post(params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
