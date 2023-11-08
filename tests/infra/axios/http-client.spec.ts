8cc6eea897d99a2a389120e51c2fb4c23646977b
wanderson aragão <wanderson.eq2011@gmail.com>
2023-11-08 08:58:43 -0300

    feat: ensure AxiosHttpCLient calls axios with correct body

--- tests/infra/axios/http-client.spec.ts
+++ tests/infra/axios/http-client.spec.ts
@@ -7,22 +7,26 @@ const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('AxiosHttpClient', () => {
   let url: string
+  let body: object
   let sut: AxiosHttpClient
   beforeAll(() => {
     url = faker.internet.url()
+    body = {
+      any_body: faker.string.alpha()
+    }
   })
   beforeEach(() => {
     jest.clearAllMocks()
     sut = new AxiosHttpClient()
   })
-  it('should call axios with correct URL and make a post', async () => {
-    await sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+  it('should call axios with correct URL and body to call a post', async () => {
+    await sut.post({ url, body })
+    expect(mockedAxios.post).toHaveBeenCalledWith(url, body)
   })
 })
 
 export class AxiosHttpClient {
   async post(postParams: HttpPostParams<any>): Promise<void> {
-    await axios.post(postParams.url)
+    await axios.post(postParams.url, postParams.body)
   }
 }
