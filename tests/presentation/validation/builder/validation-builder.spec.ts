69855dcd7e3d08c946f88094093709a62fbbb313
wanderson aragão <wanderson.eq2011@gmail.com>
2023-12-06 17:26:38 -0300

    feat: ensure validation builder return all validations

--- tests/presentation/validation/builder/validation-builder.spec.ts
+++ tests/presentation/validation/builder/validation-builder.spec.ts
@@ -13,4 +13,12 @@ describe('ValidationBuilder', () => {
     const validations = ValidationBuilder.of('any_field').minLength(5).build()
     expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
   })
+  it('Should return a list of validations', () => {
+    const validations = ValidationBuilder.of('any_field').required().emailValidation().minLength(5).build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation('any_field'),
+      new EmailValidation('any_field'),
+      new MinLengthValidation('any_field', 5)
+    ])
+  })
 })
