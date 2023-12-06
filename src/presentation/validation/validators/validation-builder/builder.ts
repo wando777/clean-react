27f1ba34ea3e8fa146f744a251d32bd4a4cd0821
wanderson aragão <wanderson.eq2011@gmail.com>
2023-12-06 17:00:07 -0300

    feat: ensure validation builder return EmailValidation

--- src/presentation/validation/validators/validation-builder/builder.ts
+++ src/presentation/validation/validators/validation-builder/builder.ts
@@ -1,5 +1,6 @@
 import { FieldValidation } from "../../protocols/field-validation"
-import { RequiredFieldValidation } from "../required-field/validation"
+import { RequiredFieldValidation, EmailValidation } from ".."
+
 
 export class ValidationBuilder {
   private constructor(
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
   required(): ValidationBuilder {
     this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  emailValidation(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName))
     return this
   }
 
--- tests/presentation/validation/builder/validation-builder.spec.ts
+++ tests/presentation/validation/builder/validation-builder.spec.ts
@@ -1,8 +1,12 @@
-import { ValidationBuilder, RequiredFieldValidation } from "@/presentation/validation/validators"
+import { ValidationBuilder, RequiredFieldValidation, EmailValidation } from "@/presentation/validation/validators"
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredFieldValidation', () => {
     const validations = ValidationBuilder.of('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+  it('Should return EmailValidation', () => {
+    const validations = ValidationBuilder.of('any_field').emailValidation().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
